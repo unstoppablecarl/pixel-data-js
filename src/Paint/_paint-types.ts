55b5d6f08ed4a46401cdd29e1d248e7b51dd63b5
Carl Olsen <unstoppablecarlolsen@gmail.com>
2026-04-07 17:34:53 -0400

    progress

--- src/Paint/_paint-types.ts
+++ src/Paint/_paint-types.ts
@@ -1,15 +1,29 @@
 import type { AlphaMask, BinaryMask } from '../_types'
 
-interface BasePaintMask {
+export enum PaintMaskOutline {
+  MASKED,
+  CIRCLE,
+  RECT,
+}
+
+interface BasePaintMask<T extends PaintMaskOutline = PaintMaskOutline> {
+  readonly outlineType: T
   readonly centerOffsetX: number
   readonly centerOffsetY: number
 }
 
-export interface PaintAlphaMask extends BasePaintMask, AlphaMask {
+export interface PaintAlphaMask<T extends PaintMaskOutline = PaintMaskOutline> extends BasePaintMask<T>, AlphaMask {
 }
 
-export interface PaintBinaryMask extends BasePaintMask, BinaryMask {
+export interface PaintBinaryMask<T extends PaintMaskOutline = PaintMaskOutline> extends BasePaintMask<T>, BinaryMask {
 }
 
-export type PaintMask = PaintAlphaMask | PaintBinaryMask
+export type PaintMask = PaintAlphaMask<any> | PaintBinaryMask<any>
 
+export type PaintCursor = {
+  outlineType: PaintMaskOutline
+  centerOffsetX: number
+  centerOffsetY: number
+  w: number
+  h: number
+}
